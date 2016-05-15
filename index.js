4eddbc09153db495ff95522442ffa2349ce7c87b
Sam Kelleher <sam@samkelleher.com>
2016-05-15 14:18:37 +0100

    For containers that contain more than 5,000 files, the tool will now correctly download all files rather than stop after 5,000 by handling the continuationToken.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 'use strict';
 
 var azure = require('azure-storage');
-var debug = require('debug')('App:Index');
+var debug = require('debug')('Controller');
+var debugFile = require('debug')('RemoteFile');
+var debugDirectory = require('debug')('Directory');
+var debugLocalFile = require('debug')('LocalFile');
 var fs = require('fs');
 var path = require('path');
 var _ = require('lodash');
@@ -33,13 +36,14 @@ function beginSync(config) {
             return;
         }
 
-        var processBatch = function (containerName, continuationToken) {
+        var processBatch = function (containerName, continuationToken = null) {
 
             return new Promise(function (resolve, reject) {
 
-                var storeDirectory = `${config.storeDirectory}${config.storageAccountName}/${containerName}/`;
+                const storeDirectory = `${config.storeDirectory}${config.storageAccountName}/${containerName}/`;
 
-                fs.readdir(storeDirectory, function (err, files) {
+                fs.lstat(storeDirectory, (err, storeDirectoryStats) => {
+
                     if (err) {
                         if (err.code === 'ENOENT') {
                             debug(`Container ${containerName} does not exist locally, will not sync this container, create the folder if you want to sync it.`);
@@ -49,70 +53,90 @@ function beginSync(config) {
                         resolve();
                         return;
                     }
-                    debug(`Container ${containerName} found, it contains ${files.length} files, and will now sync...`);
 
-                    blobService.listBlobsSegmented(containerName, continuationToken, {maxResults: 5000}, function (error, result) {
+                    fs.readdir(storeDirectory, (err, files) => {
 
-                        if (error) {
-                            debug(`Get Blobs Error (${containerName}):`, error);
-                            reject();
-                            return;
-                        }
+                        debug(`${storeDirectoryStats.isSymbolicLink() ? 'Symbolic ' : ''}Container ${containerName} found, it contains ${files.length} files, and will now sync...`);
 
-                        let queue = Promise.resolve();
+                        blobService.listBlobsSegmented(containerName, continuationToken, {maxResults: 5000}, (error, result) => {
 
-                        // debug(`Blobs in ${containerResult.name}`, result.entries);
+                            if (error) {
+                                debug(`Get Blobs Error (${containerName}):`, error);
+                                reject();
+                                return;
+                            }
 
-                        debug(`Found the first ${result.entries.length} items in container ${containerName}`);
+                            let queue = Promise.resolve();
 
-                        _.forEach(result.entries, function (blobResult) {
-                            //debug(`Examining ${blobResult.name} (${blobResult.properties['content-type']} ${blobResult.properties['content-length']} bytes)`);
+                            const resolution = {
+                                continuationToken: result.continuationToken,
+                                queue: queue,
+                                localCopiesAlreadyExisted: 0,
+                                containerName
+                            };
 
-                            var localFilePath = `${storeDirectory}${blobResult.name}`;
+                            // debug(`Blobs in ${containerResult.name}`, result.entries);
 
-                            queue = queue.then(() => {
-                                return new Promise((downloadFinished, downloadFailed) => {
-                                    fs.access(localFilePath, fs.F_OK, function (err, stats) {
-                                        if (err) {
-                                            if (err.code === 'ENOENT') {
-                                                debug(`${blobResult.name} > ${localFilePath} - TODO`);
+                            debugDirectory(`Found the first ${result.entries.length} items in container ${containerName}`);
 
-                                                blobService.getBlobToStream(containerName, blobResult.name, fs.createWriteStream(localFilePath), {}, function (error, result, response) {
-                                                    if (error) {
-                                                        debug(`${blobResult.name} > ${localFilePath} - FAILED`, err);
+                            _.forEach(result.entries, function (blobResult) {
+                                //debug(`Examining ${blobResult.name} (${blobResult.properties['content-type']} ${blobResult.properties['content-length']} bytes)`);
+
+                                var localFilePath = `${storeDirectory}${blobResult.name}`;
+
+                                queue = queue.then(() => {
+                                    return new Promise((downloadFinished, downloadFailed) => {
+                                        fs.access(localFilePath, fs.F_OK, function (err, stats) {
+                                            if (err) {
+                                                if (err.code === 'ENOENT') {
+                                                    debugFile(`${blobResult.name} > ${localFilePath} - TODO`);
+
+                                                    blobService.getBlobToStream(containerName, blobResult.name, fs.createWriteStream(localFilePath), {}, function (error, result, response) {
+                                                        if (error) {
+                                                            debugFile(`${blobResult.name} > ${localFilePath} - FAILED`, err);
+                                                            downloadFinished();
+                                                            return;
+                                                        }
+                                                        debugFile(`${blobResult.name} > ${localFilePath} - COMPLETE - SUCCESS = ${response.isSuccessful}`);
                                                         downloadFinished();
-                                                        return;
-                                                    }
-                                                    debug(`${blobResult.name} > ${localFilePath} - COMPLETE - SUCCESS = ${response.isSuccessful}`);
+                                                    });
+
+                                                } else {
+                                                    debugFile(`${blobResult.name} > ${localFilePath} - ERROR: `, err);
                                                     downloadFinished();
-                                                });
-
-                                            } else {
-                                                debug(`${blobResult.name} > ${localFilePath} - ERROR: `, err);
-                                                downloadFinished();
+                                                }
+                                                return;
                                             }
-                                            return;
-                                        }
 
-                                        debug(`${blobResult.name} > ${localFilePath} - EXISTS `);
-                                        downloadFinished();
+                                            if (!config.logDownloadsOnly) {
+                                                debugLocalFile(`${blobResult.name} > ${localFilePath} - EXISTS `);
+                                            }
+                                            resolution.localCopiesAlreadyExisted += 1;
+
+                                            downloadFinished();
+                                        });
                                     });
                                 });
+
                             });
 
+                            queue = queue.then(() => {
+                                if (resolution.localCopiesAlreadyExisted > 0) {
+                                    debugDirectory(`${resolution.localCopiesAlreadyExisted} local files already existed.`);
+                                }
+                                return Promise.resolve();
+                            });
+
+                            if (result.continuationToken) {
+                                debugDirectory(`There are additional results still to load for container ${containerName}.`)
+                            }
+
+                            resolve(resolution);
 
                         });
-
-                        resolve({
-                            continuationToken: result.continuationToken,
-                            queue: queue
-                        });
-                        if (result.continuationToken) {
-                            debug(`There are additional results still to load for container ${containerName}.`)
-                        }
                     });
-                });
 
+                });
 
             });
 
@@ -128,44 +152,31 @@ function beginSync(config) {
             }
 
 
-            batchQueue = batchQueue.then(() => {
+            const processBatchQueueItem = (containerName, continuationToken = null) => {
 
-                var continuationToken = null;
-                var hasRun = false;
-
-                debug(`Starting ${containerResult.name}`);
-
-                return processBatch(containerResult.name, continuationToken)
+                return processBatch(containerName, continuationToken)
                     .then(function (batchResults) {
                         if (!batchResults) {
-                            debug(`Finished ${containerResult.name}, but no results...`);
+                            debugDirectory(`Finished ${batchResults.containerName}, but no results...`);
                             return Promise.resolve();
                         }
-                        debug(`Finished ${containerResult.name}`);
-                        hasRun = true;
-                        continuationToken = batchResults.nextContinuationToken;
-
-                        batchResults.queue = batchResults.queue.then(() => {
-                            if (continuationToken) {
-                                debug('Preparing second batch.');
-                                processBatch(containerResult.name, continuationToken)
-                                    .then(function (nextContinuationToken) {
-                                        hasRun = true;
-                                        continuationToken = nextContinuationToken;
-                                        debug('Preparing another batch.');
-
-                                        if (continuationToken) {
-
-                                        }
-                                    });
-                            }
-                        });
-
-                        return batchResults.queue;
-
+                        if (batchResults.continuationToken) {
+                            debugDirectory(`Staring next batch for ${batchResults.containerName}`);
+                            return processBatchQueueItem(containerName, batchResults.continuationToken);
+                        } else {
+                            debugDirectory(`Finished ${batchResults.containerName}`);
+                            return Promise.resolve();
+                        }
                     })
+            };
+
+            batchQueue = batchQueue.then(() => {
+
+                debugDirectory(`Starting ${containerResult.name}`);
+
+                return processBatchQueueItem(containerResult.name)
                     .catch(() => {
-                        debug(`Failed ${containerResult.name}`);
+                        debugDirectory(`Failed ${containerResult.name}`);
                         return Promise.resolve();
                     });
             });
