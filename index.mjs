a3816fa69e46f166a35cc71ef20aab8d36140ec4
Sam Kelleher <sam@samkelleher.com>
2019-04-28 21:59:49 +0100

    Write file async

--- index.mjs
+++ index.mjs
@@ -7,7 +7,6 @@ const fsPromises = fs.promises;
 const debugController = debug('Controller');
 const debugFile = debug('RemoteFile');
 const debugDirectory = debug('Directory');
-const debugLocalFile = debug('LocalFile');
 
 async function getContainers(serviceURL) {
     let marker;
@@ -28,7 +27,82 @@ async function getContainers(serviceURL) {
     return containers;
 }
 
-async function beginSync({ storageAccountName, storageAccountKey, storeDirectory, logDownloadsOnly }) {
+async function processContainer(container, storeDirectory, storageAccountName, serviceURL) {
+    const storeDirectoryWithContainer = `${storeDirectory}${storageAccountName}/${container.name}/`;
+
+    let storeDirectoryStats;
+    try {
+        storeDirectoryStats = await fsPromises.lstat(storeDirectoryWithContainer);
+    } catch (ex) {
+        if (ex.code === 'ENOENT') {
+            debugController(`Container ${container.name} does not exist locally, will not sync this container, create the folder if you want to sync it.`);
+        } else {
+            debugController(`Unable to load local container ${container.name} directory.`, ex);
+        }
+        return;
+    }
+
+    const files = await fsPromises.readdir(storeDirectoryWithContainer);
+    const filesInLocalContainer = files.length;
+    debugController(`${storeDirectoryStats.isSymbolicLink() ? 'Symbolic ' : ''}Container ${container.name} exists , it contains ${filesInLocalContainer} files, and will now sync...`);
+
+    const containerURL = Azure.ContainerURL.fromServiceURL(serviceURL, container.name);
+
+    let marker;
+    let localCopiesAlreadyExisted = 0;
+    let filesCopied = 0;
+    let filesFailed = 0;
+    do {
+        const listBlobsResponse = await containerURL.listBlobFlatSegment(
+            Azure.Aborter.none,
+            marker
+        );
+
+        marker = listBlobsResponse.nextMarker;
+        for (const blob of listBlobsResponse.segment.blobItems) {
+            let localFilePath = `${storeDirectoryWithContainer}${blob.name}`;
+
+            try {
+                await fsPromises.access(localFilePath);
+                localCopiesAlreadyExisted += 1;
+                continue;
+            } catch (err) {
+                if (err.code !== 'ENOENT') {
+                    // Swallow the 'does not exist' error, since we will create it.
+                    // otherwise we skip it.
+                    debugFile(`${blob.name} > ${localFilePath} - ERROR: `, err);
+                    filesFailed += 1;
+                    continue;
+                }
+            }
+
+            debugFile(`${blob.name} > ${localFilePath} - TODO`);
+            try {
+                const blobURL = Azure.BlobURL.fromContainerURL(containerURL, blob.name);
+                const blockBlobURL = Azure.BlockBlobURL.fromBlobURL(blobURL);
+                const downloadBlockBlobResponse = await blockBlobURL.download(Azure.Aborter.none, 0);
+                const fileStream = fs.createWriteStream(localFilePath);
+                await new Promise((writeFinished, writeFailed) => {
+                    const stream = downloadBlockBlobResponse.readableStreamBody.pipe(fileStream);
+                    stream.on('finish', () =>{
+                        writeFinished();
+                    });
+                });
+            } catch (err) {
+                debugFile(`${blob.name} > ${localFilePath} - FAILED`, err);
+                filesFailed += 1;
+                continue;
+            }
+
+            filesCopied += 1;
+            debugFile(`${blob.name} > ${localFilePath} - COMPLETE - SUCCESS`);
+        }
+    } while (marker);
+
+    debugDirectory(`${localCopiesAlreadyExisted} local files already existed for ${container.name}, ${filesCopied} files copied, ${filesFailed} files failed.`);
+}
+
+async function beginSync({ storageAccountName, storageAccountKey, storeDirectory }) {
     const sharedKeyCredential = new Azure.SharedKeyCredential(storageAccountName, storageAccountKey);
     const pipeline = Azure.StorageURL.newPipeline(sharedKeyCredential);
 
@@ -39,101 +113,18 @@ async function beginSync({ storageAccountName, storageAccountKey, storeDirectory
 
     const containers = await getContainers(serviceURL);
 
-    let processBatch = async function (container, continuationToken = null) {
-        const storeDirectoryWithContainer = `${storeDirectory}${storageAccountName}/${container.name}/`;
-
-        let storeDirectoryStats;
-        try {
-            storeDirectoryStats = await fsPromises.lstat(storeDirectoryWithContainer);
-        } catch (ex) {
-            if (ex.code === 'ENOENT') {
-                debugController(`Container ${container.name} does not exist locally, will not sync this container, create the folder if you want to sync it.`);
-            } else {
-                debugController(`Unable to load local container ${container.name} directory.`, ex);
-            }
-            return;
-        }
-
-        const files = await fsPromises.readdir(storeDirectoryWithContainer);
-        debugController(`${storeDirectoryStats.isSymbolicLink() ? 'Symbolic ' : ''}Container ${container.name} found, it contains ${files.length} files, and will now sync...`);
-
-        const containerURL = Azure.ContainerURL.fromServiceURL(serviceURL, container.name);
-
-        let marker;
-        let localCopiesAlreadyExisted = 0;
-        do {
-            const listBlobsResponse = await containerURL.listBlobFlatSegment(
-                Azure.Aborter.none,
-                marker
-            );
-
-            marker = listBlobsResponse.nextMarker;
-            for (const blob of listBlobsResponse.segment.blobItems) {
-                console.log(`Blob: ${blob.name}`);
-
-                let localFilePath = `${storeDirectory}${blob.name}`;
-
-                try {
-                    await fsPromises.access(localFilePath);
-                } catch (err) {
-                    if (err.code !== 'ENOENT') {
-                        debugFile(`${blob.name} > ${localFilePath} - ERROR: `, err);
-                    }
-                    continue;
-                }
-
-                debugFile(`${blob.name} > ${localFilePath} - TODO`);
-                const downloadBlockBlobResponse = await blob.download(Azure.Aborter.none, 0);
-
-                continue;
-
-                blobService.getBlobToStream(container.name, blob.name, fs.createWriteStream(localFilePath), {}, function (error, result, response) {
-                    if (error) {
-                        debugFile(`${blob.name} > ${localFilePath} - FAILED`, err);
-                        downloadFinished();
-                        return;
-                    }
-                    debugFile(`${blob.name} > ${localFilePath} - COMPLETE - SUCCESS = ${response.isSuccessful}`);
-                    downloadFinished();
-                });
-            }
-        } while (marker);
-
-        if (localCopiesAlreadyExisted > 0) {
-            debugDirectory(`${localCopiesAlreadyExisted} local files already existed for ${container.name}.`);
-        }
-    };
-
-    const processBatchQueueItem = async (container, continuationToken = null) => {
-        return processBatch(container, continuationToken)
-            .then(function (batchResults) {
-                if (!batchResults) {
-                    debugDirectory(`Finished ${container.name}, but no results...`);
-                    return Promise.resolve();
-                }
-                if (batchResults.continuationToken) {
-                    debugDirectory(`Staring next batch for ${container.name}`);
-                    return processBatchQueueItem(container, batchResults.continuationToken);
-                } else {
-                    debugDirectory(`Finished ${container.name}`);
-                    return Promise.resolve();
-                }
-            })
-    };
-
     for (const container of containers) {
         if (container.name === '$root') {
             debugController('Skipping root container.');
             continue;
         }
 
-        debugDirectory(`Starting ${container.name}`);
-
         try {
-            await processBatchQueueItem(container)
+            debugDirectory(`Starting ${container.name}.`);
+            await processContainer(container, storeDirectory, storageAccountName, serviceURL);
+            debugDirectory(`Finished ${container.name}.`);
         } catch (ex) {
             debugDirectory(`Failed ${container.name}`, ex);
-            continue;
         }
     }
 
